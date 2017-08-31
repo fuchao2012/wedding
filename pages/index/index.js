2c064d0aa1e8098e74803e91ef92fbf990a1c4ac
zheng <zfc2012@126.com>
2017-09-01 01:42:12 +0800

    init project

---
+++ pages/index/index.js
@@ -0,0 +1,66 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        motto: 'Hello Fuchao',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        markers: [{
+            iconPath: "/resources/marker.png",
+            id: 0,
+            latitude: 41.309972,
+            longitude: 123.788066,
+            width: 25,
+            height: 30
+        }]
+    },
+    //事件处理函数
+
+    onLoad: function() {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    console.log(res.userInfo)
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
+    },
+    getUserInfo: function(e) {
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
+    },
+    bindTapEvent() {
+        wx.openLocation({
+            latitude: 41.309986,
+            longitude: 123.788066,
+            name: "万豪国际酒店",
+            address: "辽宁省本溪市明山区地工路43号",
+        })
+    }
+})
