b02c117bd03ea4b37b265c617e73c38a32b071c2
zheng <zfc2012@126.com>
2017-09-07 22:31:16 +0800

    CHANGE: clean  code

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,6 @@ const app = getApp()
 
 Page({
     data: {
-        motto: 'Hello Fuchao',
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -25,15 +24,15 @@ Page({
                 userInfo: app.globalData.userInfo,
                 hasUserInfo: true
             })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
+        // } else if (this.data.canIUse) {
+        //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        //     // 所以此处加入 callback 以防止这种情况
+        //     app.userInfoReadyCallback = res => {
+        //         this.setData({
+        //             userInfo: res.userInfo,
+        //             hasUserInfo: true
+        //         })
+        //     }
         } else {
             // 在没有 open-type=getUserInfo 版本的兼容处理
             wx.getUserInfo({
@@ -62,5 +61,21 @@ Page({
             name: "万豪国际酒店",
             address: "辽宁省本溪市明山区地工路43号",
         })
+    },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
     }
+    return {
+      title: '请 柬',
+      path: 'pages/index/index',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  }
 })
