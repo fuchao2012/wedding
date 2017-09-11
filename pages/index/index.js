67880f271feb5181fb5a33366f2759a8a34b2323
zheng <zfc2012@126.com>
2017-09-11 22:28:20 +0800

    remove title
    change
    
    1. 请柬居中
    2. 添加黄色黑体标题
    3. 开头空两格
    4. 内容使用宋体
    5. 名字位置互换
    6. 请去掉
    7. 地图尺寸更换
    8. 下午16时演出开始

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,17 +24,7 @@ Page({
                 userInfo: app.globalData.userInfo,
                 hasUserInfo: true
             })
-        // } else if (this.data.canIUse) {
-        //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        //     // 所以此处加入 callback 以防止这种情况
-        //     app.userInfoReadyCallback = res => {
-        //         this.setData({
-        //             userInfo: res.userInfo,
-        //             hasUserInfo: true
-        //         })
-        //     }
         } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
             wx.getUserInfo({
                 success: res => {
                     app.globalData.userInfo = res.userInfo
@@ -69,13 +59,7 @@ Page({
     }
     return {
       title: '请 柬',
-      path: 'pages/index/index',
-      success: function (res) {
-        // 转发成功
-      },
-      fail: function (res) {
-        // 转发失败
-      }
+      path: 'pages/index/index'
     }
   }
 })
